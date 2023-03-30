3b6d24cc51a3f50df44a9830d2f64a885a573757
Ekansh Rajpal <ekanshrajpal13@gmail.com>
2023-03-30 17:52:25 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var userModel = require("./users");
 
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+router.get('/create', function (req, res, next) {
+  res.render('sign');
+});
+
+
+
+router.post('/create', function (req, res, next) {
+  var cr = {
+    username:req.body.username,
+    password:req.body.password,
+  }
+  userModel.create(cr).then(function(de){
+    res.send(de);
+  })
+});
+
+
+router.get('/info', function (req, res, next) {
+  userModel.find().then(function(de){
+    res.send(de);
+  })
+});
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,52 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://localhost/spotifyclone").then(function () {
+  console.log("connect");
 });
 
-module.exports = router;
+
+var userSchema = mongoose.Schema({
+  name: String,
+  username: String,
+  passport: String,
+  email: String,
+  birth: String,
+  gender: String,
+  userdp: String,
+  isadmin: {
+    type: Boolean,
+    default: false
+  },
+  productid: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "songs"
+  }],
+  playlist: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "playlist"
+  }],
+  listcounter: {
+    type: Number,
+    default: 1
+  },
+  address: String,
+  contactnumber: Number,
+  pic: {
+    type: String,
+    default: ""
+  },
+  createdadminsongs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "songs"
+  }],
+  adminExtra: String,
+  adminExtra2: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "songs"
+  }]
+
+});
+
+
+module.exports = mongoose.model("user", userSchema);
+
